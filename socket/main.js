a4c0cb932cbd465d439d274273865e07b1e89615
Zepurts <53970116+Akshat120@users.noreply.github.com>
2024-02-18 13:23:31 +0000

    update: redis client

--- socket/main.js
+++ socket/main.js
@@ -3,65 +3,70 @@ const { createServer } = require('node:http');
 const { Server } = require("socket.io");
 
 const redis = require('redis');
-const redisClient = redis.createClient(process.env.REDIS_URL);
-
-redisClient.on('error', (err) => {
-    console.error('Redis error:', err);
+const redisClient = redis.createClient({
+    url: process.env.REDIS_URL,
+    socket: {
+        tls: true,
+        rejectUnauthorized: false // Consider setting to true in production for enhanced security
+    }
 });
 
-redisClient.on('connect', () => {
+// Connect to the Redis server
+redisClient.connect().then(() => {
     console.log('Connected to Redis');
+}).catch((err) => {
+    console.error('Redis connection error:', err);
 });
 
 exports.mainSocket = (app)=>{
-    const server = createServer(app);
-    const io = new Server(server);
-    const db = require('../models/db')
+        const server = createServer(app);
+        const io = new Server(server);
+        const db = require('../models/db')
+        
+        io.on('connection', (socket) => {
+            socket.on('register', userId => {
+                redisClient.setEx(userId, 60*60*24, socket.id).then(_data=>{
+                    console.log(`${userId} is stored in redis having value ${socket.id}`)
+                }).catch(err=>{
+                    console.log("err",err)
+                })
+            });
 
-    io.on('connection', (socket) => {
-        socket.on('register', userId => {
-            redisClient.setEx(userId, 60*60*24, socket.id).then(_data=>{
-                console.log(`${userId} is stored in redis having value ${socket.id}`)
-            }).catch(err=>{
-                console.log("err",err)
-            })
+            socket.on('chat message', async (from_id, to_id, msg, callback) => {
+                await redisClient.get(to_id).then(socketId=>{
+                    console.log(socketId)
+                    io.to(socketId).emit('received-message', from_id, msg) // 'value' or null if the key doesn't exist or expired
+                }).catch(err=>{
+                    console.error(err);
+                    console.log(`User ${to_id} not connected.`);
+                })
+
+                await db.sendChat(from_id,to_id,msg).then(_data=>{
+                    callback();
+                }).catch(err=>{
+                    console.log('error:',err)
+                })
+
+            });
+
+            // socket.on('disconnect', () => {
+            //     // Find the user associated with the disconnected socket and remove them
+
+            //     client.del("key", function(err, response) {
+            //         if (err) {
+            //           console.error("Error:", err);
+            //         } else {
+            //           console.log("Number of keys removed:", response);
+            //         }
+            //       });
+
+            //     const userId = Object.keys(userId_socketId_map).find(key => userId_socketId_map[key] === socket.id);
+            //     if (userId) {
+            //         delete userId_socketId_map[userId]; // Remove the user from the map
+            //         console.log(`User ${userId} disconnected.`);
+            //     }
+            // });
         });
 
-        socket.on('chat message', async (from_id, to_id, msg, callback) => {
-            await redisClient.get(to_id).then(socketId=>{
-                console.log(socketId)
-                io.to(socketId).emit('received-message', from_id, msg) // 'value' or null if the key doesn't exist or expired
-            }).catch(err=>{
-                console.error(err);
-                console.log(`User ${to_id} not connected.`);
-            })
-
-            await db.sendChat(from_id,to_id,msg).then(_data=>{
-                callback();
-            }).catch(err=>{
-                console.log('error:',err)
-            })
-
-        });
-
-        // socket.on('disconnect', () => {
-        //     // Find the user associated with the disconnected socket and remove them
-
-        //     client.del("key", function(err, response) {
-        //         if (err) {
-        //           console.error("Error:", err);
-        //         } else {
-        //           console.log("Number of keys removed:", response);
-        //         }
-        //       });
-
-        //     const userId = Object.keys(userId_socketId_map).find(key => userId_socketId_map[key] === socket.id);
-        //     if (userId) {
-        //         delete userId_socketId_map[userId]; // Remove the user from the map
-        //         console.log(`User ${userId} disconnected.`);
-        //     }
-        // });
-    });
-
-    return server
+        return server
 }
